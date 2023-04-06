d681b08a92a8a3d948a080e3831844d9416f7440
Dementevms <dementevms@gmail.com>
2023-04-06 10:49:44 +0500

    feat(components): copy Button on Input

---
+++ packages/components/Input/lib/Input.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+import { Props } from "./types";
+
+export const Input: FC<Props> = ({ className }) => {
+  return <input />;
+};
---
+++ packages/components/Input/stories/Input.stories.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Input } from "../lib/";
+
+export default { title: "Input" };
+
+export const primary = () => <Input />;
