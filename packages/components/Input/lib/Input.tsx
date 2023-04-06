6bc42b7a3492353339586bb978794fe9881d70d2
Dementevms <dementevms@gmail.com>
2023-04-06 10:59:55 +0500

    feat(components): add external control to Input

--- packages/components/Input/lib/Input.tsx
+++ packages/components/Input/lib/Input.tsx
@@ -1,11 +1,29 @@
-import React, { ChangeEvent, FC } from "react";
+import React, { ChangeEvent, FC, useEffect, useState } from "react";
 
 import { Props } from "./types";
 
-export const Input: FC<Props> = ({ className, value, onChange }) => {
+export const Input: FC<Props> = ({
+  className,
+  value: externalValue,
+  onChange,
+}) => {
+  const [value, setValue] = useState<string | number>();
+
+  useEffect(() => {
+    if (externalValue !== undefined && String(externalValue) !== value) {
+      setValue(externalValue);
+    }
+  }, [externalValue]);
+
+  useEffect(() => {
+    if (value !== undefined && onChange) {
+      onChange(String(value));
+    }
+  }, [value]);
+
   function handleChange(event: ChangeEvent<HTMLInputElement>): void {
-    if (onChange) onChange(event.target.value);
+    setValue(event.target.value);
   }
 
-  return <input onChange={handleChange} />;
+  return <input className={className} value={value} onChange={handleChange} />;
 };
